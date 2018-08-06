e93b0bac8186480f4412d18736112781195a1803
Stephen Hung <hung.stephen.yh@gmail.com>
2018-08-06 16:18:40 -0700

    Added Postgres DB

--- index.js
+++ index.js
@@ -4,20 +4,36 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 const app = express()
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req,res) => res.send(cool()))
-  .get('/times', (req, res) => {
-    let result = ''
-    const times = process.env.TIMES || 5
-    for (i = 0; i < times; i++){
-      result += i + ' '
-    }
-    res.send(result)
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/cool', (req,res) => res.send(cool()))
+    .get('/times', (req, res) => {
+        let result = ''
+        const times = process.env.TIMES || 5
+        for (i = 0; i < times; i++){
+            result += i + ' '
+        }
+        res.send(result)
+    })
+    .get('/db', async (req, res) => {
+        try{
+            const client = await pool.connect()
+            const result = await client.query('SELECT * FROM test_table');
+            res.render('pages/db', result);
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
