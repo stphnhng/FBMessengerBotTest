77cabc8d83e4bed46f53ded8632f9aaecd7d8a25
Stephen Hung <hung.stephen.yh@gmail.com>
2018-08-06 15:40:37 -0700

    Added TIMES env var

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
+const app = express()
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -9,4 +11,13 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req,res) => res.send(cool()))
+  .get('/times', (req, res) => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++){
+      result += i + ' '
+    }
+    res.send(result)
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
