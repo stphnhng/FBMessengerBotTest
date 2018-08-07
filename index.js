bc0db841306ec6e966a5952e8469c2f0d657f3b8
Stephen Hung <hung.stephen.yh@gmail.com>
2018-08-06 17:27:16 -0700

    trying to fix postgres -internal server error

--- index.js
+++ index.js
@@ -7,33 +7,39 @@ const app = express()
 const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl:true,
 });
 
-express()
+pool.connect();
+
+app
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', (req,res) => res.send(cool()))
-    .get('/times', (req, res) => {
-        let result = ''
-        const times = process.env.TIMES || 5
-        for (i = 0; i < times; i++){
-            result += i + ' '
-        }
-        res.send(result)
-    })
-    .get('/db', async (req, res) => {
-        try{
-            const client = await pool.connect()
-            const result = await client.query('SELECT * FROM test_table');
-            res.render('pages/db', result);
-            client.release();
-        } catch (err) {
-            console.error(err);
-            res.send("Error " + err);
-        }
-    })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.get('/', (req, res) => res.render('pages/index'))
+
+app.get('/cool', (req,res) => res.send(cool()))
+
+app.get('/times', (req, res) => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++){
+        result += i + ' '
+    }
+    res.send(result)
+  })
+
+app.get('/db', async (req, res) => {
+    try{
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM test_table');
+        res.render('pages/db', result);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
 
