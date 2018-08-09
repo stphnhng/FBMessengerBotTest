2dcf6c69c7f02b22e38281b74fc2f5a18aa3b441
Stephen Hung <hung.stephen.yh@gmail.com>
2018-08-08 18:54:15 -0700

    more resetting to find message sending bug

--- index.js
+++ index.js
@@ -170,7 +170,35 @@ const askTemplate = (text) => {
         }
     }
 }
+
+// Sends response messages via the Send API
+const callSendAPI = (sender_psid, response, cb = null) => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    };
  
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": config.get('facebook.page.access_token') },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            if(cb){
+                cb();
+            }
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
+/*
 // Sends response messages via the Send API
 const callSendAPI = (sender_psid, response, cb = null) => {
     // Construct the message body
@@ -201,5 +229,5 @@ const callSendAPI = (sender_psid, response, cb = null) => {
         }
     });
 }
-
+*/
 
