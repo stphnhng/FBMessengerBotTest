45166c516fd4d6a3dab6f048ed95b38633130241
Stephen Hung <hung.stephen.yh@gmail.com>
2018-08-08 19:36:24 -0700

    defaulted post method

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
 
+/*
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
  
@@ -45,6 +46,43 @@ app.post('/webhook', (req, res) => {
  
         // Returns a '200 OK' response to all requests
         console.log("200 status sent");
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
+ 
+});*/
+
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+ 
+    let body = req.body;
+ 
+    if (body.object === 'page') {
+ 
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+ 
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+ 
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+ 
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                console.log(webhook_event.message)
+            } else if (webhook_event.postback) {
+                console.log(webhook_event.postback)
+            }
+        });
+ 
+        // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
     } else {
         // Returns a '404 Not Found' if event is not from a page subscription
